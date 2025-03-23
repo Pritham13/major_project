c96b7b523c60b8f6f6b27a48158ee0cad125e5a8
Pritham13 <prithamhsuvarna@gmail.com>
2025-03-23 11:27:16 +0530

    adding states enum for response state machine

--- packages/fsm_pkg.sv
+++ packages/fsm_pkg.sv
@@ -2,9 +2,16 @@ package fsm_pkg;
   
   // enums for request fsm 
   typedef enum {
-    INITIAL_ST ,
-    SAMPLE_DATA_ST,
+    INITIAL_REQ_ST ,
+    SAMPLE_DATA_REQ_ST,
     WRT_REQ_ST,
-    WAIT_ST
+    WAIT_REQ_ST
     } req_states_e ;
+    // enums for response fsm 
+    typedef enum {
+    INITIAL_RESP_ST,
+    FIFO_RREQ_RESP_ST,
+    FIFO_SAMPLE_DATA_ST,
+    DRIVE_FLIT_OUT_ST
+      } resp_states_e;
 endpackage
