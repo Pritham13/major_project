ddace902b853eb3506e9b812b2ac795b2c4ea31d
Pritham13 <prithamhsuvarna@gmail.com>
2025-04-26 19:48:03 +0530

    update filelist to include additional directories and files

--- compile/filelist.f
+++ compile/filelist.f
@@ -1,7 +1,11 @@
 +incdir+${PKG_DIR}
++incdir+${TEST_DIR}
 ${PKG_DIR}/fsm_pkg.sv
 ${PKG_DIR}/ni_pkg.sv
 ${PKG_DIR}/apb_pkg.sv
 ${RTL_DIR}/ni.sv
 ${RTL_DIR}/apb_master.sv
+${RTL_DIR}/top.sv
+${RTL_DIR}/fifo.sv
+${TB_DIR}/top_tb.sv
 
